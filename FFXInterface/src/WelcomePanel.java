f101e0e8a6f909b57b5128b0a852d18d891e89b6
jillhauer <jillhauer@wireless-nat-inside.uiowa.edu>
2016-01-31 15:04:08 -0600

    merge - added mainDriver, welcome pane finished, background set

--- FFXInterface/src/WelcomePanel.java
+++ FFXInterface/src/WelcomePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
@@ -9,35 +11,64 @@ public class WelcomePanel extends JPanel{
 	
 	private JTextArea title;
 	private JTextArea email;
+	private JTextArea jobName;
+	private JPanel blank1;
+	private JPanel blank2;
 	private JTextField emailField;
+	private JTextField jobNameField;
 	private GridLayout layout;
 	
 	public WelcomePanel()
 	{
 		super();
+		setBackground(Color.WHITE);
 		
 		// setting panel layout, 1 column and three rows
-		layout = new GridLayout(3, 1, 4, 4);
+		layout = new GridLayout(7 , 1, 2, 0);
 		setLayout(layout);
 		
 		// row one, user information title
 		title = new JTextArea();
-		title.setText("User Information\n");
+		title.setText(" User Information"); 
 		title.setEditable(false);
+		title.setFont(new Font("Calibri", Font.BOLD, 20));
 		add(title);
 		
 		// row two, email address
 		email = new JTextArea();
-		email.setText("Email: ");
+		email.setText(" Email: ");
 		email.setEditable(false);
+		email.setFont(new Font("Calibri", Font.PLAIN, 20));
 		add(email);
 		
-		// row two, email field
+		// row three, email field
 		emailField = new JTextField();
 		emailField.setBounds(30, 30, 40, 30);
 		emailField.setEditable(true);
 		add(emailField);
-			
+		
+		// row four, space fill
+		blank1 = new JPanel();
+		blank1.setBackground(Color.WHITE);
+		add(blank1);
+		
+		//row five, space fill
+		blank2 = new JPanel();
+		blank2.setBackground(Color.WHITE);
+		add(blank2);
+		
+		//row five, job name
+		jobName = new JTextArea();
+		jobName.setText(" Job Name:");
+		jobName.setEditable(true);
+		jobName.setFont(new Font("Calibri", Font.PLAIN, 18));
+		add(jobName);
+		
+		//row six, job name field
+		jobNameField = new JTextField();
+		jobNameField.setBounds(30, 30, 40, 30);
+		jobNameField.setEditable(true);
+		add(jobNameField);	
 	}
 	
 
