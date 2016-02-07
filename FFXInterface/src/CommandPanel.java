97d56bfd2504fc90c20c46497630c35d46a91985
Austin <Austin@wireless-nat-inside.uiowa.edu>
2016-02-07 10:41:07 -0600

    Updated Command Panel

--- FFXInterface/src/CommandPanel.java
+++ FFXInterface/src/CommandPanel.java
@@ -12,8 +12,15 @@ import java.awt.Font;
 public class CommandPanel extends JPanel{
 	
 	private JTextArea title;
+	private JTextArea blankspace1;
+	private JTextArea blankspace2;
+	private JTextArea commandoptiondescriptons;
+	private JTextArea anneal;
+	private JTextArea energy;
+	private JTextArea md;
+	private JTextArea minimize;
 	private GridLayout layout;
-	private String[] FFXCommand = { "[FFX Command]", "Minimize", "Rotomer Optimize"};
+	private String[] FFXCommand = { "[FFX Command]", "Anneal", "Engergy", "MD","Minimize" };
 	private JComboBox commandSelect= new JComboBox(FFXCommand);
 	
 
@@ -24,9 +31,16 @@ public class CommandPanel extends JPanel{
 		setBackground(Color.WHITE);	
 		
 		//Setting Panel Layout
-		layout = new GridLayout(2,1,5,5);
+		layout = new GridLayout(20,0,3,3);
 		setLayout(layout);
 		
+		//Blank Space Holder #1
+		blankspace1 = new JTextArea();
+		blankspace1.setText(" ");
+		blankspace1.setFont(new Font("Calibri", Font.PLAIN, 18));
+		blankspace1.setEditable(false);
+		add(blankspace1);
+				
 		//Select Command Label
 		title = new JTextArea();
 		title.setText(" Select Command:");
@@ -38,12 +52,58 @@ public class CommandPanel extends JPanel{
 		commandSelect.setSelectedIndex(0);
 		commandSelect.setFont(new Font("Calibri", Font.PLAIN, 16));
 		add(commandSelect);
-	
+		
+		//Blank Space Holder #2
+		blankspace2 = new JTextArea();
+		blankspace2.setText(" ");
+		blankspace2.setFont(new Font("Calibri", Font.PLAIN, 18));
+		blankspace2.setEditable(false);
+		add(blankspace2);
+		
+		//Command Option Descriptions Title
+		commandoptiondescriptons = new JTextArea();
+		commandoptiondescriptons.setText(" Command Descriptions-");
+		commandoptiondescriptons.setFont(new Font("Calibri", Font.PLAIN, 18));
+		commandoptiondescriptons.setEditable(false);
+		add(commandoptiondescriptons);
+		
+		
+		//Command and Purpose Anneal
+		anneal = new JTextArea();
+		anneal.setText(" Anneal - Run simulated annealing to optimize a structure.");
+		anneal.setFont(new Font("Calibri", Font.PLAIN, 16));
+		anneal.setEditable(false);
+		add(anneal);
+		
+		//Command and Purpose Energy
+		energy = new JTextArea();
+		energy.setText(" Energy - Calculate the AMOEBA potential energy.");
+		energy.setFont(new Font("Calibri", Font.PLAIN, 16));
+		energy.setEditable(false);
+		add(energy);
+		
+		//Command and Purpose MD
+		md = new JTextArea();
+		md.setText(" MD - Run NVE or NVT molecular dynamics.");
+		md.setFont(new Font("Calibri", Font.PLAIN, 16));
+		md.setEditable(false);
+		add(md);
+		
+		//Command and Purpose MD
+		minimize = new JTextArea();
+		minimize.setText(" Minimize - Minimize a structure under the AMOEBA potential.");
+		minimize.setFont(new Font("Calibri", Font.PLAIN, 16));
+		minimize.setEditable(false);
+		add(minimize);
+		
+		
+		
 		
 		//Advanced Options Button
-		button = new JButton("Advanced Options");
-		button.setFont(new Font("Calibri", Font.PLAIN, 18));
-		add(button);
+		//button = new JButton("Advanced Opti	ons");
+		//button.setBounds(15,15,30,30);
+		//button.setFont(new Font("Calibri", Font.PLAIN, 18));
+		//add(button);
 		
 
 	}
